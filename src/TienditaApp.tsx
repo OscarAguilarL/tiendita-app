692affefe81729b033fda9409a3ce3308613e33a
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-02-01 08:56:50 -0600

    Proyect init

---
+++ src/TienditaApp.tsx
@@ -0,0 +1,9 @@
+export const TienditaApp = () => {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold underline">
+        Tiendita App
+      </h1>
+    </div>
+  );
+};
